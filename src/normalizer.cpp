a99cb40d64e39fe6ca118acdbd7cf62536f6a4c5
nk125 <nekes125@outlook.com>
2024-06-05 19:13:18 -0600

    fix(normalizer): Fix an invalid inserter

--- src/normalizer.cpp
+++ src/normalizer.cpp
@@ -7,7 +7,7 @@
 std::string_view self::normalizer::normalize(std::string_view word) {
 	std::string norm_word;
 
-	std::transform(word.begin(), word.end(), std::back_inserter(norm_word.begin()), 
+	std::transform(word.begin(), word.end(), std::inserter(norm_word, norm_word.end()), 
 	[](unsigned char ch) {
 		return static_cast<char>(toupper(ch));
 	});
